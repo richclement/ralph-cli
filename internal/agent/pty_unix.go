3be6e09258bcfb2eb2f1c1cb279c6652278e0530
Rich Clement <rclement@sonatype.com>
2026-01-08 07:51:11 -0500

    fix to PTY EOF detection for unit tests

--- internal/agent/pty_unix.go
+++ internal/agent/pty_unix.go
@@ -4,9 +4,10 @@ package agent
 
 import (
 	"errors"
+	"os"
 	"syscall"
 )
 
 func isPTYEOF(err error) bool {
-	return errors.Is(err, syscall.EIO)
+	return errors.Is(err, syscall.EIO) || errors.Is(err, os.ErrClosed)
 }
